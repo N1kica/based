0c1a0d37d0261d23713ca07a8c4372002026a12b
Nik Korošec <nik.korosec@gorenje.com>
2024-07-18 15:18:12 +0200

    added icon component

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,44 +1,42 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { IconComponent } from '../../shared/icon.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, IconComponent],
   template: `
     <header>
       <div
-        class="flex items-center justify-between h-16 px-6 sm:px-12 bg-gradient-to-r from-cyan-500 to-blue-500 text-white"
+        class="flex items-center justify-between h-16 px-6 sm:px-10 bg-gradient-to-r from-cyan-500 to-blue-500 text-white"
       >
         <a routerLink="/"
-          ><img src="assets/logo-nav.png" alt="angular" class="h-10"
+          ><img src="assets/logo.svg" alt="angular" class="size-10"
         /></a>
-        <nav id="menu">
-          <ul role="list" class="hidden sm:flex">
+        <nav>
+          <ul role="list" class="hidden sm:flex font-medium">
             <li>
               <a
                 routerLink="/"
-                class="px-4 py-2 hover:text-gray-200 transition-colors"
+                class="px-4 py-2 hover:text-gray-300 transition-colors"
                 >HOME</a
               >
             </li>
             <li>
               <a
                 routerLink="/page"
-                class="px-4 py-2 hover:text-gray-200 transition-colors"
+                class="px-4 py-2 hover:text-gray-300 transition-colors"
                 >PAGE</a
               >
             </li>
           </ul>
         </nav>
-        <button
-          data-collapse-toggle="menu"
-          type="button"
-          aria-controls="menu"
-          aria-expanded="false"
-          class="block sm:hidden cursor-pointer"
-        >
-          <img src="assets/list.svg" class="h-5" />
+        <button class="block sm:hidden cursor-pointer">
+          <app-icon
+            [icon]="'fb-list'"
+            [styles]="'size-6 text-white hover:text-gray-300 transition-colors'"
+          />
         </button>
       </div>
     </header>
